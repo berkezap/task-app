c8b7b85baa54a399306e26b16b5dd0654200aac0
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-09-14 02:08:28 +0300

    create form

---
+++ task-application/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div>
+      <TaskCreate />
+      <h1>Görevlerim</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
---
+++ task-application/src/components/TaskCreate.jsx
@@ -0,0 +1,16 @@
+function TaskCreate() {
+  return (
+    <div>
+      <h3>Lütfen Task Ekleyiniz!</h3>
+      <form className="task-form">
+        <label>Başlık</label>
+        <input />
+        <label>Task Giriniz</label>
+        <textarea />
+        <button>Oluştur</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskCreate;
