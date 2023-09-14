e365a3c10eda034fd11e458d41844b66cb190721
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-09-14 19:45:54 +0300

    listing

--- task-application/src/components/TaskList.jsx
+++ task-application/src/components/TaskList.jsx
@@ -1,15 +1,13 @@
 import TaskShow from "./TaskShow";
 
-function TaskList({tasks}) {
-  return <div>
-    {
-        tasks.map((task, index)=>{
-            return(
-              <TaskShow key={index} task={task}/>
-            );
-        })
-    }
-  </div>;
+function TaskList({ tasks }) {
+  return (
+    <div className="task-list">
+      {tasks.map((task, index) => {
+        return <TaskShow key={index} task={task} />;
+      })}
+    </div>
+  );
 }
 
 export default TaskList;
--- task-application/src/components/TaskShow.jsx
+++ task-application/src/components/TaskShow.jsx
@@ -1,7 +1,16 @@
 function TaskShow({ task }) {
-    console.log(task);
-  return <div>
-
-  </div>;
+  console.log(task);
+  return (
+    <div className="task-show">
+      <h3 className="task-title">Göreviniz</h3>
+      <p>{task.title}</p>
+      <h3 className="task-title">Yapılacaklar</h3>
+      <p>{task.taskDesc}</p>
+      <div>
+        <button className="delete-button">Sil</button>
+        <button className="update-button">Güncelle</button>
+      </div>
+    </div>
+  );
 }
 export default TaskShow;
